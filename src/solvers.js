cc246a5805ab0df38a2caa67b278402c6627eb6f
kidmillions <claymanchris@gmail.com>
2015-05-18 16:48:59 -0700

    Almost passed test.

--- src/solvers.js
+++ src/solvers.js
@@ -14,7 +14,12 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+
+  var solution = new Board({n: n});
+
+
+
+
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -24,7 +29,36 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var board = new Board({n: n});
+
+  var findAllNonConflicts = function(currentRow, currentBoard, numRooks) {
+    //if at end of the decision tree, no more rooks
+      //increment solution count
+
+    if (numRooks === 0) {
+      solutionCount++;
+      return;
+    }
+
+
+    for(var i = 0; i < currentBoard.rows().length; i++){
+      //clear the row
+      if (!(currentBoard.hasColConflictAt(i))) {
+        currentBoard.togglePiece(currentRow, i);
+        numRooks--;
+        debugger;
+        findAllNonConflicts(currentRow + 1, currentBoard, numRooks);
+        currentBoard.togglePiece(currentRow, i);
+      }
+
+      //if there is conflict with the column
+        //
+
+    }
+  };
+
+  findAllNonConflicts(0, board, n);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
